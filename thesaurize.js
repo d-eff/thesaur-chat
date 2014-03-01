b7d4232803faa6fefdbb533264f21faf38313a93
David F <dfernandez06@gmail.com>
2014-03-01 16:15:25 -0500

    Added comments to thesaurize

--- thesaurize.js
+++ thesaurize.js
@@ -11,32 +11,41 @@ var collection = db.get(config.dbcollection),
     //input = process.argv[2].split(' ');
 
 //TODO: GET A NEW API
-//oh god, I don't remember how this thing works
 function splitWordList(word, data){
+  
   var wordList = [];
+  
+  //API response is: {response: {list: {synonyms: "syn1|syn2|..."}}}
+  //I don't know why.
   if(data.response){
   data.response.forEach(function(respo){
     respo.list.synonyms.split('|')
     .forEach(function(d){
+      //sometimes the response terms are: syn1 (similar term)
+      //this just discards anything funky after the term
       var w = d.split(' ');
         wordList.push(w[0]);
     })
   });
   } else {
+    //this never fires, because we catch the error in getWord
+    //and never call splitWordList, but just to be safe
     wordList.push(word);
   }
   var rand = Math.random()*wordList.length|0;
   var h = wordList[rand];
+
+  //again, just in case
   if(h !== undefined){
     output[word] = h;
   }
+
+  //if we had to fetch from the api, push the new term to the db
   collection.insert({"term":word, "synonyms":wordList}, function(e, docs){
     if(e){
       console.log("error in insert " + e);
     }
   });
-   
-  
 };
 
 //TODO: Add local cache
@@ -46,8 +55,12 @@ function splitWordList(word, data){
 //if not, call api
 //  if we call api, write data to local + remote caches
 function getWord(word, callback){
+  //sets the word as a property of the output obj
+  //initially output was an array, but the callbacks don't
+  //always fire in order - making it an obj allows us to preserve that
   output[word] = word;
-  console.log("GETTING WORD");
+
+  //check the db
   collection.find({"term": word}, {}, 
   function(e, docs){
     if(e){
@@ -55,19 +68,20 @@ function getWord(word, callback){
     } else {
       //we got a hit in the db
       if(docs.length > 0){
+        //get a random synonym
         output[word] = docs[0].synonyms[Math.random()*docs[0].synonyms.length|0];
         callback();
       } else {
-        //if docs came back empty, check api
-        console.log("checking API");
+        //if we didn't find it in the db, check api
         var url = "http://thesaurus.altervista.org/service.php?word="+word+"&language=en_US&output=json&key="+config.apiKey;
-        request(url, 
-        function(error, response, body){
+        request(url, function(error, response, body){
           if(!error && response.statusCode == 200){
             //we got a hit in the api 
+            //we need to parse the api response
             splitWordList(word, JSON.parse(body));
             
           } else {
+            //nothing found, so we'll just use the word itself
             console.log("API call error " + error);
           }
           //async callback;
@@ -92,6 +106,7 @@ async.each(input, getWord, function(err){
   if(err){
     console.log("problem");
   } else {
+    //we've got all the words, put them back together
     var b = [];
     for(var x in output){
       b.push(output[x]);
@@ -101,5 +116,5 @@ async.each(input, getWord, function(err){
 });
 };
 
-  db.close();
+db.close();
 module.exports = xpo;
